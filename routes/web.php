855c8cb0c31f04cdf28a888978558cee7209dc35
bbrillianta <brilliantabintangvirgantara@gmail.com>
2021-06-20 12:33:45 +0700

    meggyy

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/tes', function () {
+        return view('tes');
+    });
+});
+
